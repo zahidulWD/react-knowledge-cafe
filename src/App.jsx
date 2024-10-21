1ea2bc29094d69712e1e48e0c7243969f47a0883
zahidul <zahidul670791@gmail.com>
2024-10-21 08:40:59 -0700

    react and tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+
+      <Header></Header>
+      <Blogs></Blogs>
+
+    </>
+  )
+}
+
+export default App
